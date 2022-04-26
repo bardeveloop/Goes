e190d335e935ae18f54241068c30ffea8695a142
Bardev <taureuswft@gmail.com>
2022-04-26 09:49:21 -0500

    mi primer commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main // declarar un package el nombre de la carpeta en la que esta gueardado
+
+import "fmt" // importa libreria para imprimir
+
+func main() { // crea una funcion
+
+    fmt.Println("Hello, World!")
+
+}
+
+// go build pos para compilar we
+
+// go run para correr el programa we
+
+// wp
